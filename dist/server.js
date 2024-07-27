687a61be7725b44b22f64c626a772381def51313
mramzannasir <mramzannasir555@gmail.com>
2024-07-27 18:35:59 +0500

    updated new

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
 import { app } from "./app.js";
 import { connectDB } from "./utils/connectDB.js";
 connectDB();
-app.listen(4040, () => console.log("Server is running on port 4040..."));
+app.listen(3000, () => console.log("Server is running on port 3000..."));
--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import { connectDB } from "./utils/connectDB.js";
 
 connectDB();
 
-app.listen(4040, () => console.log("Server is running on port 4040..."));
+app.listen(3000, () => console.log("Server is running on port 3000..."));
