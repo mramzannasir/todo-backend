d7c2394ede1cd4d6399bec102aed34f5447cb1b8
mramzannasir <mramzannasir555@gmail.com>
2024-07-29 09:17:46 +0500

    add cors middleware

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,17 @@ import userRoutes from "./routes/user.js";
 import todoRoutes from "./routes/todo.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 export const app = express();
+app.use(cors({
+    origin: "http://your-frontend-domain.com",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/todo", todoRoutes);
 app.get("/", (req, res) => {
-    res.send("Backend start from here");
+    res.send("Welcome to the Todo API! ^^");
 });
